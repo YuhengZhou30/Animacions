c275f2a35da86dc9c1efeb8860752a0dce3ea4b0
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-04-09 19:02:15 +0200

    pepe

--- core/src/com/yuheng/animation/Animator.java
+++ core/src/com/yuheng/animation/Animator.java
@@ -13,44 +13,37 @@ import com.badlogic.gdx.math.Vector3;
 
 public class Animator implements ApplicationListener {
 
-	Rectangle up, down, left, right, fire;
-	final int IDLE=0, UP=1, DOWN=2, LEFT=3, RIGHT=4;
-	// Constant rows and columns of the sprite sheet
+	Rectangle left, right;
+	final int IDLE = 0, LEFT = 3, RIGHT = 4;
 	private static final int FRAME_COLS = 35, FRAME_ROWS = 29;
-
-	// Objects used
-	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+	private static final float BACKGROUND_SCROLL_SPEED = 500.0f;
+	float lastTouchX = -1;
+	float lastTouchY = -1;
+	Animation<TextureRegion> walkAnimation;
 	Texture walkSheet;
 	SpriteBatch spriteBatch;
-	private OrthographicCamera camera;
-	// A variable for tracking elapsed time for the animation
+	TextureRegion currentFrame;
+	OrthographicCamera camera;
 	float stateTime;
-
-	// Background texture
 	Texture backgroundTexture;
+	float backgroundOffsetX = 0;
+
 	@Override
 	public void create() {
-		// Crea la cámara
 		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 
-		// Carga la hoja de sprites como una textura
 		walkSheet = new Texture(Gdx.files.internal("Orangeblack.png"));
 		backgroundTexture = new Texture(Gdx.files.internal("background.jpg"));
-		int SCR_WIDTH = Gdx.graphics.getWidth();
-		int SCR_HEIGHT = Gdx.graphics.getHeight();
 
-		// Define los rectángulos para controlar el movimiento del personaje
-		up = new Rectangle(0, SCR_HEIGHT * 3 / 4, SCR_WIDTH, SCR_HEIGHT / 4);
-		down = new Rectangle(0, 0, SCR_WIDTH, SCR_HEIGHT / 4);
-		left = new Rectangle(0, 0, SCR_WIDTH / 4, SCR_HEIGHT);
-		right = new Rectangle(SCR_WIDTH * 3 / 4, 0, SCR_WIDTH / 4, SCR_HEIGHT);
 
+		left = new Rectangle(0, -600, 400, 550);
+		right = new Rectangle(0, -50, 400, 550);
 
 		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
 				walkSheet.getWidth() / FRAME_COLS,
 				walkSheet.getHeight() / FRAME_ROWS);
 
-		// Coloca las regiones en un arreglo unidimensional en el orden correcto
 		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
 		int index = 0;
 		for (int i = 0; i < FRAME_ROWS; i++) {
@@ -58,63 +51,52 @@ public class Animator implements ApplicationListener {
 				walkFrames[index++] = tmp[i][j];
 			}
 		}
-		backgroundTexture.setWrap(Texture.TextureWrap.MirroredRepeat,
-				Texture.TextureWrap.MirroredRepeat);
-		// Inicializa la animación con el intervalo de fotogramas y el arreglo de fotogramas
-		walkAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
+		backgroundTexture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
 
-		// Instancia un SpriteBatch para dibujar y reinicia el tiempo de animación acumulado a 0
+		walkAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
 		spriteBatch = new SpriteBatch();
 		stateTime = 0f;
 
-		// Instancia un nuevo objeto Animator
 		Animator animator = new Animator();
-		// Establece la cámara en el objeto Animator
 		animator.setCamera(camera);
 	}
 
-
 	@Override
-	public void resize(int width, int height) {
 
-	}
-
-	@Override
 	public void render() {
-		// Clear screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		int joystickDirection = virtual_joystick_control();
-		// Draw background
+
 		spriteBatch.begin();
-		spriteBatch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		spriteBatch.end();
 
-		// Accumulate elapsed animation time
+		// Dibuja el fondo repetido e infinito
+		spriteBatch.draw(backgroundTexture, backgroundOffsetX, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		spriteBatch.draw(backgroundTexture, backgroundOffsetX + Gdx.graphics.getWidth(), 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 		stateTime += Gdx.graphics.getDeltaTime();
+		currentFrame = walkAnimation.getKeyFrame(stateTime, true); // Asignación única de currentFrame
 
-		// Get current frame of animation for the current stateTime
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
-
-		// Calculate the position to draw the character to center it on the screen
 		float characterX = (Gdx.graphics.getWidth() - currentFrame.getRegionWidth() * 4) / 2;
 		float characterY = (Gdx.graphics.getHeight() - currentFrame.getRegionHeight() * 4) / 5;
 
-		spriteBatch.begin();
-		// Draw current frame at the centered position
+		// Dibuja el personaje con la opción de voltear horizontalmente
 		spriteBatch.draw(currentFrame, characterX, characterY, currentFrame.getRegionWidth() * 4, currentFrame.getRegionHeight() * 4);
 		spriteBatch.end();
 	}
 
 
+
 	@Override
 	public void pause() {
-
 	}
 
 	@Override
 	public void resume() {
+	}
 
+	@Override
+	public void resize(int width, int height) {
 	}
 
 	@Override
@@ -124,51 +106,48 @@ public class Animator implements ApplicationListener {
 	}
 
 	protected int virtual_joystick_control() {
-		// Obtener las coordenadas del toque actual
 		float touchX = Gdx.input.getX();
 		float touchY = Gdx.input.getY();
 
-		// Imprimir las coordenadas del toque actual
-		System.out.println("Click"+ " Touch X: " + touchX + ", Touch Y: " + touchY);
-
-		// Si la pantalla está en orientación horizontal, ajustar las coordenadas
 		if (Gdx.graphics.getWidth() > Gdx.graphics.getHeight()) {
 			float temp = touchX;
 			touchX = touchY;
-			touchY = Gdx.graphics.getWidth() - temp; // Ajustar y invertir
+			touchY = Gdx.graphics.getWidth() - temp;
 		} else {
-			touchY = Gdx.graphics.getHeight() - touchY; // Invertir y
+			touchY = Gdx.graphics.getHeight() - touchY;
 		}
 
-		// Imprimir las coordenadas del toque después del ajuste
-		//System.out.println("Click "+ "Adjusted Touch X: " + touchX + ", Adjusted Touch Y: " + touchY);
-		// Traducir las coordenadas de la pantalla a las coordenadas del mundo del juego
 		Vector3 touchPos = new Vector3(touchX, touchY, 0);
 		camera.unproject(touchPos);
 
-		// Verificar si el toque está dentro de alguna de las áreas de control
+		if (touchPos.x != lastTouchX || touchPos.y != lastTouchY) {
+			// Las coordenadas han cambiado desde la última vez
 
-		if (up.contains(touchPos.x, touchPos.y)) {
-			System.out.println("Click"+ " Up");
-			return UP;
-		} else if (down.contains(touchPos.x, touchPos.y)) {
-			System.out.println("Click"+" Down");
-			return DOWN;
-		} else if (left.contains(touchPos.x, touchPos.y)) {
-			System.out.println("Click"+" Left");
-			return LEFT;
-		} else if (right.contains(touchPos.x, touchPos.y)) {
-			System.out.println("Click "+ "Right");
-			return RIGHT;
-		} else {
-			//System.out.println("Click"+ " No area clicked");
+			if (left.contains(touchPos.x, touchPos.y)) {
+				System.out.println("Click Left");
+				backgroundOffsetX += BACKGROUND_SCROLL_SPEED * Gdx.graphics.getDeltaTime();
+				if (currentFrame != null) {
+					currentFrame.flip(true, false);
+				}
+				lastTouchX = touchPos.x;
+				lastTouchY = touchPos.y;
+				return LEFT;
+			} else if (right.contains(touchPos.x, touchPos.y)) {
+				System.out.println("Click Right");
+				backgroundOffsetX -= BACKGROUND_SCROLL_SPEED * Gdx.graphics.getDeltaTime();
+				if (currentFrame != null) {
+					currentFrame.flip(true, false);
+				}
+				lastTouchX = touchPos.x;
+				lastTouchY = touchPos.y;
+				return RIGHT;
+			}
 		}
-
+		lastTouchX = touchPos.x;
+		lastTouchY = touchPos.y;
 		return IDLE;
+
 	}
-
-
-
 
 	public void setCamera(OrthographicCamera camera) {
 		this.camera = camera;
